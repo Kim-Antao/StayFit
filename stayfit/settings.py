8c7ffb0d5f301d2dcfffbb3b171956bfb8b3d5cb
Kim-Antao <kimberley2609@gmail.com>
2021-06-09 12:33:04 +0000

    static and media files location added

--- stayfit/settings.py
+++ stayfit/settings.py
@@ -204,8 +204,7 @@ if 'USE-AWS' in os.environ:
     # override the static and media
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
-
+   
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
